7b1a44e49441187bf67c09311f628c2fb6f63d20
Benjamin Macher <benjamin.dev@icloud.com>
2020-04-22 10:39:09 +0200

    tooling: tsconfig.build.jsons extend tsconfig.json in every package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors')
+import express from 'express';
+import cors from 'cors';
 import { sayHello } from '@mevn/common';
 
 const app = express();
